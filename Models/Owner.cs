0701714f795b553b84f0342066d6a38728054b35
GiovanaJensen <oliveirasfernandes143@gmail.com>
2024-06-22 16:46:53 -0300

    add: models

---
+++ Models/Owner.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace revisandoApis.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+    }
+}
